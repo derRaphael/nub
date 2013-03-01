cd1b01d83dab3390400dcf306abeb5d7fce5dd30
derRaphael <software@itholic.org>
2013-03-01 14:30:05 +0100

    fixed index.html and main.js to be interoperable with new samples

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -1,38 +1,11 @@
-(function(){
-    var el;
-    var dragger = function () {
-        this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
-        this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
-        this.animate({"fill-opacity": .2}, 500);
-    },
-    move = function (dx, dy) {
-        var att = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
-        this.attr(att);
-        for (var i = connections.length; i--;) {
-            r.connection(connections[i]);
-        }
-        r.safari();
-    },
-    up = function () {
-        this.animate({"fill-opacity": 0}, 500);
-    },
+var r, icons_g, icons_s;
+
+$(document).ready(function(){
+
     r = Raphael("holder", "100%", "100%"),
     icons_g = Raphael("icons-generic", "100%", "100%"),
-    icons_s = Raphael("icons-specific", "100%", "100%"),
-    connections = [],
-    shapes = [  r.ellipse(190, 100, 30, 20),
-                r.rect(290, 80, 60, 40, 10),
-                r.rect(290, 180, 60, 40, 2),
-                r.ellipse(450, 100, 20, 20)
-            ];
-    for (var i = 0, ii = shapes.length; i < ii; i++) {
-        var color = Raphael.getColor();
-        shapes[i].attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
-        shapes[i].drag(move, dragger, up);
-    }
-    connections.push(r.connection(shapes[0], shapes[1], "#fff"));
-    connections.push(r.connection(shapes[1], shapes[2], "#fff", "#fff|5"));
-    connections.push(r.connection(shapes[1], shapes[3], "#000", "#fff"));
+    icons_s = Raphael("icons-specific", "100%", "100%");
+
 
     var holder_w = $('#holder').width(), 
         holder_h = $('#holder').height(), 
@@ -105,9 +78,9 @@
         $.each(collection,function(index1,elements){
             $.each(elements,function(index2,element){
                 element.hover( icon_mouseover, icon_mouseout ).attr({cursor:'pointer',title:index1});
-            })
+            });
         });
-    })
+    });
 
     $(window).resize(
         function(){
@@ -128,5 +101,7 @@
             holder_icons['Move Left'][1].attr( {transform: ['t',5,holder_h/2-20]} );
             r.safari();
         }
-    )
-})();
+    );
+
+
+});
