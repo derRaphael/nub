689ba41eb95f4b4aead903d0e02ca8cc4af5af53
derRaphael <software@itholic.org>
2013-03-02 01:01:54 +0100

    process flow arrow design

--- ui/assets/js/raphael/connection.js
+++ ui/assets/js/raphael/connection.js
@@ -77,4 +77,96 @@ Raphael.fn.connection = function (obj1, obj2, line, bg) {
     }
 };
 
+// from https://gist.github.com/viking/1043360
+Raphael.fn.arrow = function(x1, y1, x2, y2, size, arrow, style, attr) {
 
+    var start = "", end = "";
+
+    if ( typeof arrow == "undefined" || arrow != "none" )
+    {
+        var angle = Raphael.angle(x1, y1, x2, y2);
+        var a35   = Raphael.rad(angle-35);
+        var a35m  = Raphael.rad(angle+35);
+        var a145  = Raphael.rad(angle-145);
+        var a145m = Raphael.rad(angle+145);
+        var x1a = x1 + Math.cos(a145) * size;
+        var y1a = y1 + Math.sin(a145) * size;
+        var x1b = x1 + Math.cos(a145m) * size;
+        var y1b = y1 + Math.sin(a145m) * size;
+        var x2a = x2 + Math.cos(a35) * size;
+        var y2a = y2 + Math.sin(a35) * size;
+        var x2b = x2 + Math.cos(a35m) * size;
+        var y2b = y2 + Math.sin(a35m) * size;
+    }
+    if ( typeof arrow == "undefined" || arrow == "both" || arrow == "start" )
+    {
+
+        start = "M"+x1a+" "+y1a+"L"+x1+" "+y1+"L"+x1b+" "+y1b;
+
+        if ( typeof style == "undefined" || style != "simple" )
+        {
+            start += "L"+x1a+" "+y1a;
+
+            var tmp = Raphael.pathIntersection( start, "M"+x1+" "+y1+"L"+x2+" "+y2 );
+
+            _x1 = tmp[tmp.length-1].x;
+            _y1 = tmp[tmp.length-1].y;
+
+            start = "M" + _x1 + " " + _y1 + "L" + x1a + " " + y1a + "L" + x1 + " " + y1 + "L" + x1b + " " + y1b + "L" + _x1 + " " + _y1;
+
+            x1 = _x1;
+            y1 = _y1;
+
+        }
+
+    }
+    if ( typeof arrow == "undefined" || arrow == "both" || arrow == "end" )
+    {
+
+        end = "M"+x2+" "+y2+"L"+x2a+" "+y2a+"M"+x2+" "+y2+"L"+x2b+" "+y2b ;
+
+        if ( typeof style == "undefined" || style != "simple" )
+        {
+            end += "L"+x2a+" "+y2a;
+
+            var tmp = Raphael.pathIntersection( end, "M"+x1+" "+y1+"L"+x2+" "+y2 );
+
+            _x2 = tmp[tmp.length-1].x;
+            _y2 = tmp[tmp.length-1].y;
+
+            end = "M" + _x2 + " " + _y2 + "L" + x2a + " " + y2a + "L" + x2 + " " + y2 + "L" + x2b + " " + y2b + "L" + _x2 + " " + _y2
+
+            x2 = _x2;
+            y2 = _y2;
+        }
+
+    }
+    if ( typeof attr != "object" )
+    {
+        return this.path( start + "M"+x1+" "+y1+"L"+x2+" "+y2+ end );
+    } else {
+        return this.path( start + "M"+x1+" "+y1+"L"+x2+" "+y2+ end ).attr( attr );
+    }
+};
+
+Raphael.fn.outlineArrow = function(x1, y1, x2, y2, size, arrow, style, attr, outlineColor) {
+
+    if ( typeof attr == "object" && attr.hasOwnProperty("outline-color") )
+    {
+        outlineColor = attr['outline-color'];
+    } else {
+        outlineColor = "#000";
+    }
+    if ( typeof attr == "object" && attr.hasOwnProperty("stroke-width") )
+    {
+        strokeWidth = attr['stroke-width'] + 3;
+    } else {
+        strokeWidth = 6;
+    }
+
+    return {
+                bg: this.arrow(x1, y1, x2, y2, size, arrow, style ).attr(attr).attr( { 'stroke-width': strokeWidth, stroke: outlineColor } ),
+                fg: this.arrow(x1, y1, x2, y2, size, arrow, style, attr )
+            };
+
+}
